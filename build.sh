782ad0f22eff6a0529b1a46d495a8865b97f9541
Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>
2024-04-07 08:19:52 -0700

    chore: worked on the dockerfile
    Signed-off-by: Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 # Navigate to the directory containing Dockerfile
 cd "$(dirname "$0")" || exit
 DOCKERFILE_DIR=$(pwd)
+IMAGE_NAME=demo
+IMAGE_TAG=v1.0.0
 
 # Build the Docker image
-docker build -t your_image_name:your_tag "$DOCKERFILE_DIR"
+docker build -t $IMAGE_NAME:$IMAGE_TAG "$DOCKERFILE_DIR"
